4729e0485819e14bd1689e88f698c9d62229e06e
SMTYON <75500482+SMTYON@users.noreply.github.com>
2025-07-31 13:23:44 +0300

    Update ordered_Sets

--- Data structures/ordered_Sets.cpp
+++ Data structures/ordered_Sets.cpp
@@ -7,3 +7,11 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 template<typename T>
 using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+void erase_one(ordered_multiset<T>& ms, const T& val) {
+    auto it = ms.lower_bound(val);
+    if (it != ms.end() && *it == val) {
+        ms.erase(it);
+    }
+}
